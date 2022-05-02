876738b0a58af4bd41e54e3916b37e11cc576a3a
cmickensj <89092260+cmickensj@users.noreply.github.com>
2022-05-01 22:58:50 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,108 @@
+/* Guess what answers you would get if you ran this in the Javascript Console in Google Chrome. Once you have an answer to the questions then check them by copying them and running it in the console yourself line by line */ 
+
+// 1. Evaluate each of the following expressions
+/* 
+
+1. 5 + "34" 
+
+2. 5 - "4"
+
+3. 10 % 5
+
+4. 5 % 10
+
+5. "Java" + "Script" 
+
+6. " " + " " 
+
+7. " " + 0 
+
+8. true + true 
+
+9. true + false 
+
+10. false + true 
+
+11. false - true 
+
+12. 3 - 4 
+
+13. "Bob" - "bill" 
+
+*/
+
+
+// 2. Evaluate each of the following comparisons. What is different about these "comparisons" and the previous "expressions?"
+/* 
+
+    1. 5 >= 1
+    2. 0 === 1 
+    3. 4 <= 1 
+    4. 1 != 1 
+    5. "A" > "B" 
+    6. "B" < "C" 
+    7. "a" > "A" 
+    8. "b" < "A" 
+    9. true === false 
+    10. true != true
+
+*/
+
+// 3. Make the string: "Hi There! It's "sunny" out" by using the + sign.
+/* 
+    Place your answer here
+
+    con
+
+// 4. add variable "firstName" and "lastName" - so that they equal your name
+/* 
+    Place your answer here
+
+*/
+
+// 5. create a variable that holds the answer // of "firstName" + " " + "lastName"
+/* 
+    Place your answer here
+
+
+*/
+
+// 6. What is a + b?
+
+/* 
+    var a = 34;
+    var b = 21;
+    a = 2;
+*/
+
+// 7. For the previous question, what is 'c' equal to?
+/* 
+    Place your answer here
+*/
+
+// 8. Make a Calculator! using prompt(), and variables, make a program that does the following:
+
+// a. Prompts the user for first number.
+// b. Stores that first number
+// c. Prompts the user for the second number.
+// d. stores that number and responds with the SUM by using an alert. 
+
+/* 
+    Place your answer here
+
+
+*/
+
+//3
+
+console.log("Hi There!" + " It's \"sunny\" out");
+
+//4
+
+var firstName = "Courtney"
+var lastName = "Mickens"
+
+console.log(firstName + lastName);
+
+//7
+
